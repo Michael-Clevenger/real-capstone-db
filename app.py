2118e83b6f1a26cfe5428753a9807abc64a108b4
michaelclevenger <clevengermichael93@yahoo.com>
2021-09-16 14:19:24 -0500

    commit

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
-heroku = Heroku(app)
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
